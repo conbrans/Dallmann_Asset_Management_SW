63431ae10e1fd0445b6347a9bc682c510d547a60
conbrans <constantin.brans@hs-osnabrueck.de>
2020-09-17 17:24:40 +0200

    Comments added

--- src/Website/routes/helproutes/crypto.js
+++ src/Website/routes/helproutes/crypto.js
@@ -1,20 +1,31 @@
 const crypto = require('crypto');
 
 const key = crypto.randomBytes(32);
-const iv = crypto.randomBytes(16);
 
+const initializationVector = crypto.randomBytes(16);
+
+/**
+ * encrypt a given text
+ * @param text, which will be encrypted
+ * @return {{encryptedData: string, initializationVector: string}}
+ */
 function encrypt(text)
 {
-    let chiper = crypto.createCipheriv('aes-256-cbc',Buffer.from(key),iv);
+    let chiper = crypto.createCipheriv('aes-256-cbc',Buffer.from(key),initializationVector);
     let encrypted = chiper.update(text);
     encrypted = Buffer.concat([encrypted,chiper.final()]);
-    return {iv:iv.toString('hex'),encryptedData : encrypted.toString('hex')};
+    return {initializationVector:initializationVector.toString('hex'),encryptedData : encrypted.toString('hex')};
 }
 
-function decrypt(text) {
-    let iv = Buffer.from(text.iv, 'hex');
-    let encryptedText = Buffer.from(text.encryptedData, 'hex');
-    let decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(key), iv);
+/**
+ * decrpyt a given jsonFile back to text
+ * @param jsonFile contains the initalization vetor and encryptedData
+ * @return {string}
+ */
+function decrypt(jsonFile) {
+    let iv = Buffer.from(jsonFile.initializationVector, 'hex');
+    let encryptedText = Buffer.from(jsonFile.encryptedData, 'hex');
+    let decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(key),initializationVector);
     let decrypted = decipher.update(encryptedText);
     decrypted = Buffer.concat([decrypted, decipher.final()]);
     return decrypted.toString();
